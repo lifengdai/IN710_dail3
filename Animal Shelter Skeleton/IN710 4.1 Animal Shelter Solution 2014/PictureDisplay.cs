4b63ead1c8edeb1cac4ffde75e56114c4156eb9b
lifengdai <d1142592239@163.com>
2016-03-11 01:08:35 +1300

    Animal Strategy Design Pattern

---
+++ Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class PictureDisplay : IDisplay
+    {
+        private List<PictureBox> listOfPictureBox;
+
+        public PictureDisplay(List<PictureBox> listOfPictureBox)
+        {
+            this.listOfPictureBox = listOfPictureBox;
+        }
+
+        /* YOUR CODE HERE */
+        public void clearDisplay()
+        {
+            foreach (PictureBox item in listOfPictureBox)
+            {
+                item.Image = null;
+            }
+        }
+
+        public void displayCritterList(List<Critter> critterList)
+        {
+            for (int i = 0; i < critterList.Count; i++)
+            {
+                listOfPictureBox[i].Image = Image.FromFile(critterList[i].ImageFileName);
+            }
+        }
+    }
+}
---
+++ Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class TextDisplay : IDisplay
+    {
+        private ListBox lb;
+
+        public TextDisplay(ListBox lb)
+        {
+            this.lb = lb;
+        }
+
+        /* YOUR CODE HERE */
+        public void clearDisplay()
+        {
+            lb.Items.Clear();
+        }
+
+        public void displayCritterList(List<Critter> critterList)
+        {
+            foreach (Critter item in critterList)
+            {
+                string str = item.Name + " " + item.Species;
+                lb.Items.Add(str);
+            }
+        }
+    }
+}
