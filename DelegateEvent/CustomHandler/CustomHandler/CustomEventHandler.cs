fe10d87d8748193e917488908ce25728b2faae2e
lifengdai <d1142592239@163.com>
2016-04-07 22:41:27 +1200

    Custom event handler

---
+++ DelegateEvent/CustomHandler/CustomHandler/CustomEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomHandler
+{
+    class CustomEventHandler
+    {
+        public delegate void customHandler(object o, EventArgs e);
+        public event customHandler handler;
+        private Random rand;
+        private Button b;
+
+        public CustomEventHandler(Button b)
+        {
+            rand = new Random();
+            this.b = b;
+        }
+
+        public void randNum(object o, EventArgs e)
+        {
+            string messgae = "My number is randnum " + rand.Next(100) + "\r\n" + "I'm from custom handler";
+            MessageBox.Show(messgae, "Information", MessageBoxButtons.OK);
+        }
+
+        public void OnClick()
+        {
+            if (handler != null)
+                handler(b, new EventArgs());
+        }
+    }
+}
---
+++ DelegateEvent/CustomHandler/CustomHandler/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CustomHandler
+{
+    public partial class Form1 : Form
+    {
+        private CustomEventHandler eh;
+
+        public Form1()
+        {
+            InitializeComponent();
+            eh = new CustomEventHandler(btnTest);
+            eh.handler += new CustomEventHandler.customHandler(eh.randNum);
+            eh.handler += new CustomEventHandler.customHandler(eh.randNum);          
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is my form's handler");
+        }
+
+        private void btnTest_MouseClick(object sender, MouseEventArgs e)
+        {
+            eh.OnClick();
+        }
+    }
+}
