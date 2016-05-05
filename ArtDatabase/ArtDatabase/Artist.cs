21a26cd4b5c60527cd04082045e5c3740392fe17
lifengdai <d1142592239@163.com>
2016-05-05 18:16:48 +1200

    Art Database

---
+++ ArtDatabase/ArtDatabase/Artist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtDatabase
+{
+    public class Artist
+    {
+        public String LastName { set; get; }
+        public String FirstName { set; get; }
+        public String Country { set; get; }
+        public int YearOfBirth { set; get; }
+        public int YearOfDeath { set; get; }
+
+        public override string ToString()
+        {
+            return (FirstName + " " + LastName + "\t\t" + YearOfBirth + "-" + YearOfDeath
+                + "\t\t" + Country);
+        }
+    }
+}
---
+++ ArtDatabase/ArtDatabase/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<Artist> artists;
+        public List<Painting> paintings;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            artists = new List<Artist>();
+            artists.Add(new Artist { Country = "France", FirstName = "Camille", LastName = "Pissarro", YearOfBirth = 1830, YearOfDeath = 1903 });
+            artists.Add(new Artist { Country = "France", FirstName = "Claude", LastName = "Monet", YearOfBirth = 1840, YearOfDeath = 1926 });
+            artists.Add(new Artist { Country = "England", FirstName = "John", LastName = "Constable", YearOfBirth = 1776, YearOfDeath = 1837 });
+            artists.Add(new Artist { Country = "Netherlands", FirstName = "Jan", LastName = "Vermeer", YearOfBirth = 1632, YearOfDeath = 1675 });
+            artists.Add(new Artist { Country = "Italy", FirstName = "Sanzio", LastName = "Raphael", YearOfBirth = 1483, YearOfDeath = 1520 });
+            artists.Add(new Artist { Country = "Spain", FirstName = "Pablo", LastName = "Picasso", YearOfBirth = 1881, YearOfDeath = 1973 });
+            artists.Add(new Artist { Country = "Norway", FirstName = "Edvard", LastName = "Munch", YearOfBirth = 1863, YearOfDeath = 1944 });
+            artists.Add(new Artist { Country = "Italy", FirstName = "Leonardo", LastName = "da Vinci", YearOfBirth = 1452, YearOfDeath = 1519 });
+            artists.Add(new Artist { Country = "Italy", FirstName = "Sandro", LastName = "Botticelli", YearOfBirth = 1445, YearOfDeath = 1510 });
+            artists.Add(new Artist { Country = "France", FirstName = "Henri", LastName = "Matisse", YearOfBirth = 1869, YearOfDeath = 1954 });
+            artists.Add(new Artist { Country = "Netherlands", FirstName = "Piet", LastName = "Mondrian", YearOfBirth = 1872, YearOfDeath = 1944 });
+            artists.Add(new Artist { Country = "United States", FirstName = "Jackson", LastName = "Pollock", YearOfBirth = 1912, YearOfDeath = 1956 });
+            artists.Add(new Artist { Country = "Netherlands", FirstName = "Vincent", LastName = "van Gogh", YearOfBirth = 1853, YearOfDeath = 1890 });
+
+            paintings = new List<Painting>();
+            paintings.Add(new Painting { Artist = "van Gogh", Title = "The Starry Night", Method = "Oil on canvas", Year = 1889, Width = 72, Height = 92 });
+            paintings.Add(new Painting { Artist = "van Gogh", Title = "Village Street in Auvers ", Method = "Oil on canvas", Year = 1890, Width = 73, Height = 92 });
+            paintings.Add(new Painting { Artist = "Pissarro", Title = "Gelee Blanche", Method = "Oil on canvas", Year = 1873, Width = 65, Height = 93 });
+            paintings.Add(new Painting { Artist = "Pissarro", Title = "Village Path", Method = "Oil on canvas", Year = 1875, Width = 72, Height = 92 });
+            paintings.Add(new Painting { Artist = "Monet", Title = "Fishing Boats Leaving the Harbor, Le Havre ", Method = "Oil on canvas", Year = 1874, Width = 60, Height = 101 });
+            paintings.Add(new Painting { Artist = "Monet", Title = "Water Lilies ", Method = "Oil on canvas", Year = 1906, Width = 88, Height = 93 });
+            paintings.Add(new Painting { Artist = "Constable", Title = "The Leaping Horse", Method = "Oil on canvas", Year = 1825, Width = 142, Height = 187 });
+            paintings.Add(new Painting { Artist = "Vermeer", Title = "Girl with a Pearl Earring", Method = "Oil on canvas", Year = 1665, Width = 45, Height = 40 });
+            paintings.Add(new Painting { Artist = "Raphael", Title = "Madonna dell Granduca ", Method = "Oil on wood", Year = 1505, Width = 84, Height = 55 });
+            paintings.Add(new Painting { Artist = "Raphael", Title = "St. George Fighting the Dragon ", Method = "Oil on wood", Year = 1825, Width = 28, Height = 21 });
+            paintings.Add(new Painting { Artist = "Munch", Title = "The Scream", Method = "Tempera on paper", Year = 1893, Width = 91, Height = 74 });
+            paintings.Add(new Painting { Artist = "da Vinci", Title = "The Last Supper", Method = "Tempera on plaster", Year = 1498, Width = 460, Height = 880 });
+            paintings.Add(new Painting { Artist = "Botticelli", Title = "The Birth of Venus", Method = "Tempera on canvas", Year = 1485, Width = 173, Height = 280 });
+            paintings.Add(new Painting { Artist = "Matisse", Title = "La Musique", Method = "Oil on canvas", Year = 1939, Width = 115, Height = 115 });
+            paintings.Add(new Painting { Artist = "Mondrian", Title = "Composition with Red, Yellow and Blue", Method = "Oil on canvas", Year = 1821, Width = 40, Height = 35 });
+            paintings.Add(new Painting { Artist = "Pollock", Title = "The Key", Method = "Oil on canvas", Year = 1946, Width = 84, Height = 213 });
+            paintings.Add(new Painting { Artist = "Picasso", Title = "The Three Musicians", Method = "Oil on canvas", Year = 1921, Width = 200, Height = 222 });
+
+
+
+        }
+
+        //------------------------------------------------------
+        // btnAllPaintings
+        //------------------------------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IEnumerable<Painting> paintings = from painting in this.paintings
+                            select painting;
+            foreach (Painting paint in paintings)
+            {
+                listBox1.Items.Add(paint.ToString());
+            }
+        }
+
+
+        //------------------------------------------------------
+        // btnArtistsFromItaly
+        //------------------------------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IEnumerable<Artist> artistsFromItaly = from artist in artists
+                                                   where artist.Country == "Italy"
+                                                   select artist;
+            foreach (Artist artist in artistsFromItaly)
+            {
+                listBox1.Items.Add(artist.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnBefore1800
+        //------------------------------------------------------
+        private void btnBefore1800_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IEnumerable<Painting> paintingBefore1800 = from painting in paintings
+                                                       where painting.Year < 1800
+                                                       select painting;
+            foreach (Painting paint in paintingBefore1800)
+            {
+                listBox1.Items.Add(paint.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnOldest
+        //------------------------------------------------------
+        private void btnOldest_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IEnumerable<Painting> OldestPainting = from painting in paintings
+                                                   orderby painting.Year
+                                                   select painting;
+            listBox1.Items.Add(OldestPainting.First().ToString());
+        }
+
+        //------------------------------------------------------
+        // byThisArtist
+        //------------------------------------------------------
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IEnumerable<Painting> paintedByThisArtist = from painting in paintings
+                                                        where painting.Artist == textBox1.Text
+                                                        select painting;
+            foreach (Painting paint in paintedByThisArtist)
+            {
+                listBox1.Items.Add(paint.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnNbyCountry
+        //------------------------------------------------------
+        private void btnNbyCountry_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var NbyCountryPainting = from painting in paintings
+                                     join artist in artists
+                                     on painting.Artist equals artist.LastName
+                                     group painting by artist.Country;
+
+            foreach (var item in NbyCountryPainting)
+            {
+                int count = item.Count();
+                string painting = "";
+                if (count == 1)
+                {
+                    painting = "painting";
+                }
+                else
+                {
+                    painting = "paintings";
+                }
+                string print = count + " " + painting + " from " + item.Key;
+                listBox1.Items.Add(print);
+            }
+        }
+
+        //------------------------------------------------------
+        // btnArtistGroupedByCountry
+        //------------------------------------------------------
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var GroupedByCountryArtists = from artist in artists
+                                          group artist by artist.Country;
+            foreach (var item in GroupedByCountryArtists)
+            {
+                listBox1.Items.Add(item.Key.ToString() + " : ");
+                foreach (var artist in item)
+                {
+                    listBox1.Items.Add("\t" + artist.LastName);
+                }
+            }                                                                         
+        }
+
+        //------------------------------------------------------
+        // btnDutchPainters
+        //------------------------------------------------------
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IEnumerable<Painting> paintingsBYDutch = from paint in paintings
+                                   join artist in artists
+                                   on paint.Artist equals artist.LastName
+                                   where artist.Country == "Netherlands"
+                                   select paint;
+
+            foreach (Painting item in paintingsBYDutch)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+        }
+
+        //------------------------------------------------------
+        // btnJoinTables
+        //------------------------------------------------------
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var joinPainting = from painting in paintings
+                               join artist in artists
+                               on painting.Artist equals artist.LastName
+                               select new { artist.FirstName, artist.LastName, artist.Country, painting.Title };
+            foreach (var item in joinPainting)
+            {
+                string print = item.FirstName + " " + item.LastName + "\t\t" + item.Country + "\t\t" + item.Title;
+                listBox1.Items.Add(print);
+            }
+        }
+
+        //------------------------------------------------------
+        // btnFrenchOrItalian
+        //------------------------------------------------------
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var paintingByFI = from painting in paintings
+                               join artist in artists
+                               on painting.Artist equals artist.LastName
+                               where artist.Country == "France" || artist.Country == "Italy"
+                               select new { painting.Artist, artist.Country, painting.Title };
+
+            foreach (var item in paintingByFI)
+            {
+                string print = item.Artist + "\t\t" + item.Country + "\t\t" + item.Title;
+                listBox1.Items.Add(print);
+            }
+        }
+    }
+}
