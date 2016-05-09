6c2d63d3a7743408b5dbccdf53bede0a85d949b0
lifengdai <d1142592239@163.com>
2016-05-09 17:53:21 +1200

    lightning strikes

---
+++ LightingStrikes/LightingStrikes/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace LightingStrikes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplayIntensity = new System.Windows.Forms.Button();
+            this.btnListRecords = new System.Windows.Forms.Button();
+            this.btnDisplayLatLongFilename = new System.Windows.Forms.Button();
+            this.btnStrikeCausedFire = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnDisplayIntensity
+            // 
+            this.btnDisplayIntensity.Location = new System.Drawing.Point(24, 18);
+            this.btnDisplayIntensity.Name = "btnDisplayIntensity";
+            this.btnDisplayIntensity.Size = new System.Drawing.Size(472, 44);
+            this.btnDisplayIntensity.TabIndex = 0;
+            this.btnDisplayIntensity.Text = "Display the average intensity";
+            this.btnDisplayIntensity.UseVisualStyleBackColor = true;
+            this.btnDisplayIntensity.Click += new System.EventHandler(this.btnDisplayIntensity_Click);
+            // 
+            // btnListRecords
+            // 
+            this.btnListRecords.Location = new System.Drawing.Point(24, 68);
+            this.btnListRecords.Name = "btnListRecords";
+            this.btnListRecords.Size = new System.Drawing.Size(472, 44);
+            this.btnListRecords.TabIndex = 0;
+            this.btnListRecords.Text = "List the complete records of lagest three fires";
+            this.btnListRecords.UseVisualStyleBackColor = true;
+            this.btnListRecords.Click += new System.EventHandler(this.btnListRecords_Click);
+            // 
+            // btnDisplayLatLongFilename
+            // 
+            this.btnDisplayLatLongFilename.Location = new System.Drawing.Point(511, 18);
+            this.btnDisplayLatLongFilename.Name = "btnDisplayLatLongFilename";
+            this.btnDisplayLatLongFilename.Size = new System.Drawing.Size(472, 44);
+            this.btnDisplayLatLongFilename.TabIndex = 0;
+            this.btnDisplayLatLongFilename.Text = "Display latitude, longitude and filename\r\n";
+            this.btnDisplayLatLongFilename.UseVisualStyleBackColor = true;
+            this.btnDisplayLatLongFilename.Click += new System.EventHandler(this.btnDisplayLatLongFilename_Click);
+            // 
+            // btnStrikeCausedFire
+            // 
+            this.btnStrikeCausedFire.Location = new System.Drawing.Point(511, 68);
+            this.btnStrikeCausedFire.Name = "btnStrikeCausedFire";
+            this.btnStrikeCausedFire.Size = new System.Drawing.Size(472, 44);
+            this.btnStrikeCausedFire.TabIndex = 0;
+            this.btnStrikeCausedFire.Text = "List all fires caused by a lightning strike";
+            this.btnStrikeCausedFire.UseVisualStyleBackColor = true;
+            this.btnStrikeCausedFire.Click += new System.EventHandler(this.btnStrikeCausedFire_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 18;
+            this.listBox.Location = new System.Drawing.Point(24, 118);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(959, 436);
+            this.listBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 571);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnStrikeCausedFire);
+            this.Controls.Add(this.btnDisplayLatLongFilename);
+            this.Controls.Add(this.btnListRecords);
+            this.Controls.Add(this.btnDisplayIntensity);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplayIntensity;
+        private System.Windows.Forms.Button btnListRecords;
+        private System.Windows.Forms.Button btnDisplayLatLongFilename;
+        private System.Windows.Forms.Button btnStrikeCausedFire;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ LightingStrikes/LightingStrikes/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightingStrikes
+{
+    public partial class Form1 : Form
+    {
+        private DataClassesDataContext data;
+
+        public Form1()
+        {
+            InitializeComponent();
+            data = new DataClassesDataContext();
+        }
+
+        private void btnDisplayIntensity_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            double average = 0;
+            IEnumerable<int> intensity = from inten in data.tblStrikes
+                                         select inten.strikeIntensity;
+            average = intensity.Average();
+            listBox.Items.Add(average);
+        }
+
+        private void btnListRecords_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            int index = 0;
+            var fireArea = from area in data.tblFires
+                           orderby area.fireArea descending
+                           select area;
+            foreach (var item in fireArea)
+            {
+                index++;
+                if (index == 4)
+                {
+                    break;
+                }
+                else
+                {
+                    string id = item.fireID.ToString();
+                    string date = item.fireDate.ToLongDateString();
+                    string lat = item.fireLatitude.ToString();
+                    string lon = item.fireLongitude.ToString();
+                    string area = item.fireArea.ToString();
+                    string fireInfo = id + "\t\t" + date + "\t\t" + lat + "\t\t" + lon + "\t\t" + area;
+                    listBox.Items.Add(fireInfo);
+                }
+            }
+        }
+
+        private void btnDisplayLatLongFilename_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            var displayLatLonFile = from light in data.tblStrikes
+                                    join pictue in data.tblPictures
+                                    on light.strikeID equals pictue.strikeID
+                                    group light by pictue.strikeID;
+            foreach (var item in displayLatLonFile)
+            {
+                foreach (var display in item)
+                {
+                    string id = display.strikeID.ToString();
+                    string lat = display.strikeLatitude.ToString();
+                    string lon = display.strikeLongitude.ToString();
+                    string filename = "";
+                    foreach (var pic in display.tblPictures)
+                    {
+                        filename = pic.pictureFileName;
+                    }
+                    string final = id + "\t\t" + lat + "\t\t" + lon + "\t\t" + filename;
+                    listBox.Items.Add(final);
+                }
+            }
+        }
+
+        private void btnStrikeCausedFire_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            var causedBylighting = from lightning in data.tblStrikes
+                                   from fire in data.tblFires
+                                   where lightning.strikeDate == fire.fireDate
+                                   && lightning.strikeLatitude == fire.fireLatitude
+                                   && lightning.strikeLongitude == fire.fireLongitude
+                                   select fire;
+            foreach (var item in causedBylighting)
+            {
+                string display = item.fireID + "\t\t" + item.fireDate + "\t\t"
+                    + item.fireLatitude + "\t\t" + item.fireLongitude + "\t\t"
+                    + item.fireArea;
+                listBox.Items.Add(display);
+            }
+        }
+    }
+}
